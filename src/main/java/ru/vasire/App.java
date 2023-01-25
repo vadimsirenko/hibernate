6b78bf1d7c973c2494d05f452f3b87d73292ff70
Сиренко Вадим Федорович <Sirenko.VF@gazprom-neft.ru>
2023-01-25 14:10:09 +0300

    Hibernate SELECT

---
+++ src/main/java/ru/vasire/App.java
@@ -0,0 +1,35 @@
+package ru.vasire;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.vasire.model.Person;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session =  sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+
+            System.out.println(person);
+
+            session.getTransaction().commit();
+        }
+        finally {
+            sessionFactory.close();
+        }
+
+    }
+}
